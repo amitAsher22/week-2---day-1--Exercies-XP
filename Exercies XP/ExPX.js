cef2da124438be0c96eee8374094683df87d534f
amita <amitasher4@gmail.com>
2021-10-11 13:32:27 +0300

    first commit

---
+++ Exercies XP/ExPX.js
@@ -0,0 +1,38 @@
+
+// 1.Guess the correct outcome to the lines of code written below.
+// 2.After guessing the outcome check the results of the code below.
+// 3.Check them by copying line by line and running them in the console.
+// Evaluate the code below:
+
+//     5 + "34"
+//     5 - "4"
+//     10 % 5
+//     5 % 10
+//     "Java" + "Script"
+//     " " + " "
+//     " " + 0
+//     true + true
+//     true + false
+//     false + true
+//     false - true
+//     3 - 4
+//     "Bob" - "bill"
+
+// answers !!!!
+
+
+
+// 5 + "34" = "534" ;
+// 5 - "4" = 1 ;
+// 10 % 5 = 0 ; 
+//  5 % 10 = 5;
+//  "Java" + "Script" = 'JavaScript';
+//  " " + " " =  ' ';
+// " " + 0 = ' 0';
+// true + true = 2;
+// true + false = 1;
+// false + true = 1;
+//  3 - 4 = -1;
+// "Bob" - "bill" = NaN;
+
+// answers !!!!
